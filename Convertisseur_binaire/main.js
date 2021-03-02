a5bad387b1ffcdd378e3953b61267283eb3e4dae
MrChuckNovice <teedji77@hotmail.fr>
2021-03-02 12:26:50 +0100

    maj_binaire

--- Convertisseur_binaire/main.js
+++ Convertisseur_binaire/main.js
@@ -1,14 +1,18 @@
 const convert = document.getElementById("convert");
 
-convert.addEventListener("click", function(){
+//recup une fois pour plusieur clique, 
+let outputCrypted= document.getElementById("crypted");
+//fonction Fléché 
+//e=event cela passe l'Evenement qui 
+convert.addEventListener("click", (e)=>{
     let inputCode= document.getElementById("msgToCode").value;
-    let outputCrypted= document.getElementById("crypted");
-     outputCrypted.value="";
+    console.log(toString.call(outputCrypted));
+    outputCrypted.value="";
            for (var i= 0; i < inputCode.length; i++) {
         outputCrypted.value += inputCode[i].charCodeAt(0).toString(2) + " ";
-        console.log(outputCrypted.value);
-       
-}
+      }   
+    outputCrypted.innerHTML = outputCrypted.value;
+    console.log(outputCrypted.value);
 });
   
 //charCodeAt = sa renvoie le code ASCII;
